b301ace3c20c4888b3a2b1e918310d2b8ba2cb4e
BertinAm <bertinfonge0@gmail.com>
2023-06-10 13:02:30 +0100

    update

--- agri_tec/App/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Farmer, Product, DeliveryService
-
-admin.site.register(Farmer)
-admin.site.register(Product)
-admin.site.register(DeliveryService)
